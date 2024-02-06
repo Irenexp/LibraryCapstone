0082a973b407344016e20c0d07f26fe4e8a98421
AndrewSwinney <andrewswinney14@gmail.com>
2024-02-06 10:54:15 +0000

    Developing_home_page

--- library-system-ui/src/App.js
+++ library-system-ui/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <MainContent/>
+      <Footer/>
     </div>
   );
 }
 
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">Library</div>
+      <div className="nav-items">
+        <a href="#home">Home</a>
+        <a href="#books">Books</a>
+        <a href="#movies">Movies</a>
+        <a href="#periodicals">Periodicals</a>
+      </div>
+      <SearchBar />
+      <UserAccount />
+    </div>
+  );
+}
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search" />
+      <button type="submit">🔍</button>
+    </div>
+  );
+}
+
+const UserAccount = () => {
+  return (
+    <div className="user-account">
+      Andrew Swinney
+    </div>
+  );
+}
+
+const MainContent = () => {
+  const logoImage = process.env.PUBLIC_URL + 'capstone_library_image.jpg';
+
+  return (
+    <div className="content-container">
+      <div className="welcome-text">
+        <h1>Welcome to Opps Library</h1>
+        <p>From the library, you can borrow books and movies.</p>
+      </div>
+      <div className="library-image">
+        <img src={logoImage} alt="Library Interior" />
+      </div>
+    </div>
+  );
+}
+
+const Footer = () => {
+
+  const [currentDate, setCurrentDate]=useState(new Date());
+  
+  useEffect(()=>{
+      const intervalId = setInterval(()=>{
+          setCurrentDate(new Date()); 
+  },1000);
+  return()=>clearInterval(intervalId);
+  }, [])
+return (
+  <footer>
+    <div className="block">
+    <div className="footer-content">
+          <span classname = "footer-text">&copy; TWG | {currentDate.getFullYear()}</span> 
+          <span classname = "footer-time"> {currentDate.toLocaleString()}</span>
+       </div>
+       </div>
+  </footer>
+);
+};
+
 export default App;
+
