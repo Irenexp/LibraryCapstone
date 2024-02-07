dc48ce27c3594a3b62f88a7278c85e8bc00923ed
twg23 <toluadetomiwa@gmail.com>
2024-02-07 08:23:04 +0000

    periodical ui updated

--- library-system-ui/src/Periodicals.js
+++ library-system-ui/src/Periodicals.js
@@ -1,9 +1,41 @@
-const Periodicals = () => {
-    return (
-      <div>
-        <h2>Periodicals Page</h2>
-      </div>
-    );
-  }
+import React, { useEffect, useState } from "react";
+import PeriodicalCard from "./PeriodicalCard";
 
-  export default Periodicals; 
+const Periodicals = () => {
+  const [periodicalList, setPeriodicalList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/books");
+
+        const data = await response.json();
+        setPeriodicalList(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("error fetching data", console.error());
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>List of Periodicals that can be viewed in the library</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {periodicalList.map((periodical) => (
+            <li key={periodical.id}>
+              {/*<p>Title: {book.title}</p>*/}
+              <PeriodicalCard periodical={periodical} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Periodicals;
